901c946b0f728707ebab3e7e1a452968a74bdbf1
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-12-04 15:35:55 +0100

    tested a monthly pay method

--- cSharpScribbles/Program.cs
+++ cSharpScribbles/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Program!");
+/*
+ calculate pay for a month. 
+       Im interested in how it changes if you increase the per/hour
+
+Monthly pay - per hour, hours
+    method(22k) = per hour and hours(month)
+    method(change per hour) = monthly pay, per hour and hours(month) 
+ */
+Pay myPay = new Pay();
+myPay.whats_my_monthly_pay(110);
+
+public class Pay
+{
+    public static int hoursPerMonth { get; set; } = 160;
+    private float moneyPerHour;
+    private float hours;
+
+    private float moneyPerMonth;
+
+    public void whats_my_hourly_pay(int monthlySalary) {
+        moneyPerHour =(float) monthlySalary / (float)hoursPerMonth;  //is this float conversion needed?
+        hours = hoursPerMonth;
+        printPerHour(moneyPerHour, hours);
+    }
+    public void whats_my_monthly_pay(int payPerHour){
+        moneyPerMonth = payPerHour * hoursPerMonth;
+        printPerHour(moneyPerMonth, hoursPerMonth);
+    }
+
+    //should i learn overide string methods?
+    private void printPerHour(float moneyPerHour, float hours)
+    {
+        Console.WriteLine($"{moneyPerHour}kr . {hours} hours.");
+    }
+
+}
+
+/*
+        C# 
+          fundamentals - book with code
+            
+    school stuff(getting LIA, assignments)
+
+    reading.. meditation and anailisum? neilisam?
+ 
+ */
