4140c9d5197f6fa66af6f917b1215a593ad015c9
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-12-08 15:20:42 +0100

    practicing Inheritance with backpack Console app

--- 01 interface Robot/Program.cs
+++ 01 interface Robot/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// MISTAKE 1 ?
+// Mistake 2. I didn't have get set in lot of the robot class...
+
+Robot robot1 = new Robot();
+robot1.Commands.Append(new RobotPowerOn());  // MISTAKE 1? With array, i tried to add an item with Append(). Does that work? 
+                                             // with append, I get: "System.NullReferenceException: 'Object reference not set to an instance of an object"
+robot1.Commands[1] = (new MoveForward());
+robot1.Commands[2] = (new MoveForward());
+
+robot1.Run();
+
 
 public class RobotPowerOn : IRobotCommands
 {
@@ -18,21 +29,22 @@ public class MoveForward : IRobotCommands
     }
 }
 
-interface IRobotCommands
+public interface IRobotCommands
 {
     void Run(Robot robot);
 }
 public class Robot
 {
-    public int X;
-    public int Y;
-    public bool IsPowered;
-    IRobotCommands[] Commands = new IRobotCommands[3];  //Q. Can I have interface as type?
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommands[] Commands { get; set; } = new IRobotCommands[3]; //Q. Can I have interface as type? Why is it good/bad?
 
     public void Run()
     {
-        foreach (IRobotCommands commands in Commands)
-        {
+        foreach (IRobotCommands commands in Commands)   //HEre is a good example of when Interface can be used as a type
+        {                                             // IRobotCommans is inherited by all commands, and that's whats 
+                                                        // needed here. So that type can be used.
             commands.Run(this);
             Console.WriteLine($"{X}, {Y}, {IsPowered}");
         }
---
+++ Inheritence Backpack/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+// New knowledge! 
+ */
+BackPack myBackPack = new(5, 4, 4);
+while (true)
+{
+    Console.WriteLine((myBackPack.currentVolume, myBackPack.currentWeight, myBackPack.currentCount));
+    string choice = Console.ReadLine();
+    InventoryItem item = choice switch   // Mistake here. Forgot that choice variable is needed. 
+    {
+        "1" => new Sword(), "2" => new shield(), "3" => new drink(),
+    };
+    if (!myBackPack.AddItem(item)) Console.WriteLine("Could not add this item");    // New knowledge! THis adds the item!
+}
+
+class BackPack
+{
+    private float maxWeight;
+    private float maxVolume;
+    private int maxCount;
+    InventoryItem[] pack;
+
+    public float currentWeight { get; private set; } = 0;
+    public float currentVolume { get; private set; } = 0;
+    public int currentCount { get; private set; } = 0;
+
+    public BackPack(float maxWeight, float maxVolume, int maxCount)
+    {
+        this.maxWeight = maxWeight;
+        this.maxVolume = maxVolume;
+        this.maxCount = maxCount;
+
+        pack = new InventoryItem[this.maxCount];
+    }
+
+    public bool AddItem(InventoryItem item)
+    {
+        if (currentWeight + item.weight > maxWeight) return false;
+        if (currentVolume + item.volume > maxVolume) return false;
+        if (currentCount  >= maxCount) return false;
+
+        currentWeight += item.weight;
+        currentVolume += item.volume;
+        pack[currentCount++] = item;
+        return true;
+    }
+}
+public class InventoryItem
+{
+    //public float weight;    //Interfaces cannot contain instance fields 
+    public float volume { get; set; }
+    public float weight { get; set; }
+    public InventoryItem(float volume, float weight) { this.volume = volume; this.weight = weight; }
+
+}
+public class Sword : InventoryItem { public Sword():base(2, 0.5f) { } }
+public class shield : InventoryItem { public shield():base(3, 3.5f) { } }
+public class drink : InventoryItem { public drink():base(0.5f, 0.5f) { } }
