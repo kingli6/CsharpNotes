93271c1c4b2e09c4da03ade8884d971defe4e2dd
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-12-19 22:45:07 +0100

    mastermind

---
+++ MasterMind/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Generate the secret code
+var code = Enumerable.Range(0, 4).Select(x => new Random().Next(1, 7)).ToArray();
+
+// Loop until the player breaks the code or runs out of turns
+for (int i = 0; i < 10; i++)
+{
+    // Prompt the player for their guess
+    Console.Write("Enter your guess (4 digits between 1 and 6): ");
+    var guess = Console.ReadLine().Trim().Select(x => x - '0').ToArray();
+
+    // Check the guess and give feedback
+    int correctDigits = code.Zip(guess, (c, g) => c == g ? 1 : 0).Sum();
+    int correctValues = code.Intersect(guess).Count() - correctDigits;
+    Console.WriteLine($"{correctDigits} correct digits in the correct place, {correctValues} correct digits in the wrong place");
+
+    // Check if the player has broken the code
+    if (correctDigits == 4)
+    {
+        Console.WriteLine("Congratulations, you broke the code!");
+        break;
+    }
+}
+        
+    
